cc9a36a25f399bac208056065f5896931d0a4e2c
Leandro Bargas <lbjbargas@gmail.com>
2020-09-16 01:05:28 -0300

    finish the project

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -34,10 +34,12 @@ function Home() {
           value={limit}
           onChange={(event) => setLimit(event.target.value)}
         >
+          <option value=" "> </option>
+          <option value="1">1</option>
+          <option value="5">5</option>
           <option value="10">10</option>
-          <option value="20">20</option>
-          <option value="30">30</option>
-          <option value="40">40</option>
+          <option value="50">50</option>
+          <option value="100">100</option>
         </select>
       </div>
 
